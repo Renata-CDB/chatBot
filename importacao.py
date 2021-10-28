771c222ab71d22df6938a436b93fdfd67953429b
Renata Bianchine <renatabianchine_@hotmail.com>
2021-10-28 16:03:25 -0300

    Estudos Modulo

---
+++ importacao.py
@@ -0,0 +1,7 @@
+import modulo
+
+print(modulo.b1, modulo.f1, modulo.variavel, modulo.numeroString, modulo.n1)
+modulo.NomeFuncao()
+modulo.OutraFuncao()
+modulo.ImprimeNome("Renata")
+modulo.ImprimeIdade(45)
---
+++ modulo.py
@@ -0,0 +1,30 @@
+#Tipos de dados
+variavel = 'casa' #string
+numeroString = '2' #string
+# Todas string é caracterizada pelas aspas
+n1 = 5 #int
+f1 = 1.5 #float
+b1 = True #bool
+
+# declara a função (define)
+def NomeFuncao():
+    print("Oi, eu sou uma função!")
+
+#NomeFuncao() #chamada da função
+
+def OutraFuncao():
+    print("oi sou outra função!")
+
+def ImprimeNome(nome):
+    # nome é o parâmetro da função
+    #um parâmetro marca as posições de um determinado valor!
+    print("Meu nome é " + nome) #concatenação
+
+# ImprimeNome("Renata") # "Renata" é um argumento da função
+# O argumento da função é posicionado nos parâmetros da definição da função
+
+"""def ImprimeIdade(idade):
+    print(idade)  """  
+
+def ImprimeIdade(idade):
+    print("Minha idade é " + str(idade) )  
